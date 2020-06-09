210ade91cdb8ec81e69a5a0ec1ff2ad66892d72f
liuyuanmu <liuyuanmu@jd.com>
2020-06-09 16:18:26 +0800

    update test for event

--- test-integration/src/test/java/test/com/jd/blockchain/intgr/IntegrationTest4Operations.java
+++ test-integration/src/test/java/test/com/jd/blockchain/intgr/IntegrationTest4Operations.java
@@ -2,24 +2,47 @@ package test.com.jd.blockchain.intgr;
 
 import com.jd.blockchain.consensus.ConsensusProviders;
 import com.jd.blockchain.consensus.bftsmart.BftsmartConsensusSettings;
-import com.jd.blockchain.crypto.*;
+import com.jd.blockchain.crypto.AsymmetricKeypair;
+import com.jd.blockchain.crypto.HashDigest;
+import com.jd.blockchain.crypto.KeyGenUtils;
+import com.jd.blockchain.crypto.PrivKey;
+import com.jd.blockchain.crypto.PubKey;
 import com.jd.blockchain.gateway.GatewayConfigProperties;
 import com.jd.blockchain.ledger.BlockchainKeypair;
+import com.jd.blockchain.ledger.DataType;
+import com.jd.blockchain.ledger.Event;
+import com.jd.blockchain.ledger.SystemEvent;
 import com.jd.blockchain.ledger.core.LedgerQuery;
+import com.jd.blockchain.sdk.SystemEventListener;
+import com.jd.blockchain.sdk.SystemEventPoint;
+import com.jd.blockchain.sdk.SystemEventPointData;
+import com.jd.blockchain.sdk.UserEventListener;
 import com.jd.blockchain.sdk.BlockchainService;
+import com.jd.blockchain.sdk.EventContext;
+import com.jd.blockchain.sdk.EventListenerHandle;
+import com.jd.blockchain.sdk.UserEventPoint;
+import com.jd.blockchain.sdk.UserEventPointData;
 import com.jd.blockchain.sdk.client.GatewayServiceFactory;
 import com.jd.blockchain.storage.service.DbConnectionFactory;
 import com.jd.blockchain.test.PeerServer;
 import com.jd.blockchain.tools.initializer.LedgerBindingConfig;
+import com.jd.blockchain.utils.Bytes;
 import com.jd.blockchain.utils.concurrent.ThreadInvoker;
+import com.jd.blockchain.utils.io.BytesUtils;
 import org.junit.Test;
 import test.com.jd.blockchain.intgr.initializer.LedgerInitializeTest;
 import test.com.jd.blockchain.intgr.initializer.LedgerInitializeWeb4Nodes;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.atomic.AtomicInteger;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 import static test.com.jd.blockchain.intgr.IntegrationBase.*;
 import static test.com.jd.blockchain.intgr.IntegrationTest4Bftsmart.DB_TYPE_ROCKSDB;
 
@@ -27,11 +50,15 @@ public class IntegrationTest4Operations {
 
     private static final String DB_TYPE_MEM = "mem";
 
-    private static final  String  BFTSMART_PROVIDER = "com.jd.blockchain.consensus.bftsmart.BftsmartConsensusProvider";
+    private static final String BFTSMART_PROVIDER = "com.jd.blockchain.consensus.bftsmart.BftsmartConsensusProvider";
 
     private static final int NEW_USER_SIZE = 10;
 
     private static final int NEW_KV_SIZE = 10;
+
+    private static final int NEW_EVENT_ACCOUNT_SIZE = 10;
+    private static final int NEW_EVENT_NAME_SIZE = 10;
+    private static final int NEW_EVENT_SEQUENCE_SIZE = 10;
 
     private boolean isRegisterUser = false;
 
@@ -41,62 +68,92 @@ public class IntegrationTest4Operations {
 
     private boolean isParticipantStateUpdate = false;
 
+    private boolean isRegisterUserEventAccount = false;
+
+    private boolean isPublishUserEvent = false;
+
+    private boolean testUserEventListener = false;
+
     private boolean isWriteKv = false;
 
     @Test
-    public void test() {
+    public void test() throws InterruptedException {
         test(LedgerInitConsensusConfig.bftsmartProvider, DB_TYPE_MEM, LedgerInitConsensusConfig.memConnectionStrings);
     }
 
     @Test
-    public void test4Rocksdb() {
+    public void test4Rocksdb() throws InterruptedException {
         test(LedgerInitConsensusConfig.bftsmartProvider, DB_TYPE_ROCKSDB, LedgerInitConsensusConfig.rocksdbConnectionStrings);
     }
 
     @Test
-    public void testUserRegister() {
+    public void testUserRegister() throws InterruptedException {
         isRegisterUser = true;
         test(LedgerInitConsensusConfig.bftsmartProvider, DB_TYPE_MEM, LedgerInitConsensusConfig.memConnectionStrings);
     }
 
     @Test
-    public void testDataAccountRegister() {
+    public void testDataAccountRegister() throws InterruptedException {
         isRegisterDataAccount = true;
         test(LedgerInitConsensusConfig.bftsmartProvider, DB_TYPE_MEM, LedgerInitConsensusConfig.memConnectionStrings);
     }
 
     @Test
-    public void testWriteKv() {
+    public void testWriteKv() throws InterruptedException {
         isRegisterDataAccount = true;
         isWriteKv = true;
         test(LedgerInitConsensusConfig.bftsmartProvider, DB_TYPE_MEM, LedgerInitConsensusConfig.memConnectionStrings);
     }
 
     @Test
-    public void testParticipantRegister() {
+    public void testParticipantRegister() throws InterruptedException {
         isRegisterParticipant = true;
         test(LedgerInitConsensusConfig.bftsmartProvider, DB_TYPE_MEM, LedgerInitConsensusConfig.memConnectionStrings);
     }
 
     @Test
-    public void testParticipantUse() {
+    public void testParticipantUse() throws InterruptedException {
         isRegisterParticipant = true;
         isParticipantStateUpdate = true;
         test(LedgerInitConsensusConfig.bftsmartProvider, DB_TYPE_MEM, LedgerInitConsensusConfig.memConnectionStrings);
     }
 
     @Test
-    public void testAll() {
+    public void testUserEventAccountRegister() throws InterruptedException {
+        isRegisterUserEventAccount = true;
+        test(LedgerInitConsensusConfig.bftsmartProvider, DB_TYPE_MEM, LedgerInitConsensusConfig.memConnectionStrings);
+    }
+
+    @Test
+    public void testPublishUserEvent() throws InterruptedException {
+        isRegisterUserEventAccount = true;
+        isPublishUserEvent = true;
+        test(LedgerInitConsensusConfig.bftsmartProvider, DB_TYPE_MEM, LedgerInitConsensusConfig.memConnectionStrings);
+    }
+
+    @Test
+    public void testEventListener() throws InterruptedException {
+        isRegisterUserEventAccount = true;
+        isPublishUserEvent = true;
+        testUserEventListener = true;
+        test(LedgerInitConsensusConfig.bftsmartProvider, DB_TYPE_MEM, LedgerInitConsensusConfig.memConnectionStrings);
+    }
+
+    @Test
+    public void testAll() throws InterruptedException {
         isRegisterUser = true;
         isRegisterDataAccount = true;
         isRegisterParticipant = true;
         isParticipantStateUpdate = true;
         isWriteKv = true;
+        isRegisterUserEventAccount = true;
+        isPublishUserEvent = true;
+        testUserEventListener = true;
 
         test(LedgerInitConsensusConfig.bftsmartProvider, DB_TYPE_MEM, LedgerInitConsensusConfig.memConnectionStrings);
     }
 
-    public void test(String[] providers, String dbType, String[] dbConnections) {
+    public void test(String[] providers, String dbType, String[] dbConnections) throws InterruptedException {
 
         final ExecutorService sendReqExecutors = Executors.newFixedThreadPool(20);
 
@@ -118,7 +175,7 @@ public class IntegrationTest4Operations {
         gwkey0.setPrivKeyValue(IntegrationBase.PRIV_KEYS[0]);
         gwkey0.setPrivKeyPassword(encodedBase58Pwd);
         GatewayTestRunner gateway = new GatewayTestRunner("127.0.0.1", 11000, gwkey0,
-                peerNodes[0].getServiceAddress(), providers,null);
+                peerNodes[0].getServiceAddress(), providers, null);
 
         ThreadInvoker.AsyncCallback<Object> gwStarting = gateway.start();
 
@@ -179,17 +236,18 @@ public class IntegrationTest4Operations {
             long participantCount = ledgerRepository.getAdminInfo(ledgerRepository.retrieveLatestBlock()).getParticipantCount();
             long userCount = ledgerRepository.getUserAccountSet(ledgerRepository.retrieveLatestBlock()).getTotal();
 
-            System.out.printf("before add participant: participantCount = %d, userCount = %d\r\n", (int)participantCount, (int)userCount);
-            IntegrationBase.KeyPairResponse participantResponse = IntegrationBase.testSDK_RegisterParticipant(adminKey, ledgerHash, blockchainService);;
+            System.out.printf("before add participant: participantCount = %d, userCount = %d\r\n", (int) participantCount, (int) userCount);
+            IntegrationBase.KeyPairResponse participantResponse = IntegrationBase.testSDK_RegisterParticipant(adminKey, ledgerHash, blockchainService);
+            ;
             participantCount = ledgerRepository.getAdminInfo(ledgerRepository.retrieveLatestBlock()).getParticipantCount();
             userCount = ledgerRepository.getUserAccountSet(ledgerRepository.retrieveLatestBlock()).getTotal();
-            System.out.printf("after add participant: participantCount = %d, userCount = %d\r\n", (int)participantCount, (int)userCount);
+            System.out.printf("after add participant: participantCount = %d, userCount = %d\r\n", (int) participantCount, (int) userCount);
 
             BftsmartConsensusSettings consensusSettings = (BftsmartConsensusSettings) ConsensusProviders.getProvider(BFTSMART_PROVIDER).getSettingsFactory().getConsensusSettingsEncoder().decode(ledgerRepository.getAdminInfo().getSettings().getConsensusSetting().toBytes());
             System.out.printf("update participant state before ,old consensus env node num = %d\r\n", consensusSettings.getNodes().length);
 
             for (int i = 0; i < participantCount; i++) {
-                System.out.printf("part%d state = %d\r\n",i, ledgerRepository.getAdminInfo(ledgerRepository.retrieveLatestBlock()).getParticipants()[i].getParticipantNodeState().CODE);
+                System.out.printf("part%d state = %d\r\n", i, ledgerRepository.getAdminInfo(ledgerRepository.retrieveLatestBlock()).getParticipants()[i].getParticipantNodeState().CODE);
             }
             if (isParticipantStateUpdate) {
                 IntegrationBase.testSDK_UpdateParticipantState(adminKey, new BlockchainKeypair(participantResponse.getKeyPair().getPubKey(), participantResponse.getKeyPair().getPrivKey()), ledgerHash, blockchainService);
@@ -197,18 +255,91 @@ public class IntegrationTest4Operations {
 
                 System.out.printf("update participant state after ,new consensus env node num = %d\r\n", consensusSettingsNew.getNodes().length);
                 for (int i = 0; i < participantCount; i++) {
-                    System.out.printf("part%d state = %d\r\n",i, ledgerRepository.getAdminInfo(ledgerRepository.retrieveLatestBlock()).getParticipants()[i].getParticipantNodeState().CODE);
+                    System.out.printf("part%d state = %d\r\n", i, ledgerRepository.getAdminInfo(ledgerRepository.retrieveLatestBlock()).getParticipants()[i].getParticipantNodeState().CODE);
                 }
             }
         }
 
-        try {
-            System.out.println("----------------- Init Completed -----------------");
-            IntegrationBase.testConsistencyAmongNodes(ledgers);
-            Thread.sleep(Integer.MAX_VALUE);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+        if (isRegisterUserEventAccount) {
+            Bytes[] eventAddress = new Bytes[NEW_EVENT_ACCOUNT_SIZE];
+            for (int m = 0; m < NEW_EVENT_ACCOUNT_SIZE; m++) {
+                KeyPairResponse response = IntegrationBase.testSDK_RegisterEventAccount(adminKey, ledgerHash, blockchainService);
+                Thread.sleep(100);
+                validKeyPair(response, ledgerRepository, IntegrationBase.KeyPairType.EVENTACCOUNT);
+                eventAddress[m] = response.keyPair.getAddress();
+            }
+
+            Map<Bytes, String[]> eventNames = new HashMap<>();
+            for (int m = 0; m < NEW_EVENT_ACCOUNT_SIZE; m++) {
+                for (int n = 0; n < NEW_EVENT_NAME_SIZE; n++) {
+                    if (n == 0) {
+                        eventNames.put(eventAddress[m], new String[NEW_EVENT_NAME_SIZE]);
+                    }
+                    eventNames.get(eventAddress[m])[n] = "event" + n;
+                }
+            }
+
+            BlockchainNewBlockEventListener blockListener = null;
+            EventListenerHandle blockListenerHandler = null;
+            // 监听事件
+            if (testUserEventListener) {
+                for (int s = 0; s < NEW_EVENT_SEQUENCE_SIZE; s++) {
+                    // 单个账户单个事件名
+                    BlockchainUserEventListener a1e1Listener = new BlockchainUserEventListener();
+                    a1e1Listener.register(eventAddress[0], eventNames.get(eventAddress[0])[0], NEW_EVENT_SEQUENCE_SIZE - 1, NEW_EVENT_SEQUENCE_SIZE - s);
+                    blockchainService.monitorUserEvent(ledgerHash, eventAddress[0].toBase58(), eventNames.get(eventAddress[0])[0], s, a1e1Listener);
+                    // 单个账户多个事件名
+                    BlockchainUserEventListener a1emListener = new BlockchainUserEventListener();
+                    UserEventPoint[] a1emEventPoints = new UserEventPoint[eventNames.size()];
+                    for (int m = 0; m < eventNames.get(eventAddress[0]).length; m++) {
+                        a1emEventPoints[m] = new UserEventPointData(eventAddress[0].toBase58(), eventNames.get(eventAddress[0])[m], s);
+                        a1emListener.register(eventAddress[0], eventNames.get(eventAddress[0])[m], NEW_EVENT_SEQUENCE_SIZE - 1, NEW_EVENT_SEQUENCE_SIZE - s);
+                    }
+                    blockchainService.monitorUserEvent(ledgerHash, a1emEventPoints, a1emListener);
+                    // 多个账户多个事件名
+                    BlockchainUserEventListener amemListener = new BlockchainUserEventListener();
+                    UserEventPoint[] amemEventPoints = new UserEventPoint[eventNames.size() * eventNames.get(eventAddress[0]).length];
+                    int m = 0;
+                    for (Map.Entry<Bytes, String[]> kv : eventNames.entrySet()) {
+                        for (int n = 0; n < kv.getValue().length; n++) {
+                            amemEventPoints[m * kv.getValue().length + n] = new UserEventPointData(kv.getKey().toBase58(), kv.getValue()[n], s);
+                            amemListener.register(kv.getKey(), kv.getValue()[n], NEW_EVENT_SEQUENCE_SIZE - 1, NEW_EVENT_SEQUENCE_SIZE - s);
+                        }
+                        m++;
+                    }
+                    blockchainService.monitorUserEvent(ledgerHash, amemEventPoints, amemListener);
+                }
+
+                blockListener = new BlockchainNewBlockEventListener();
+                blockListenerHandler = blockchainService.monitorSystemEvent(ledgerHash, new SystemEventPointData(SystemEvent.NEW_BLOCK.getName(), 0, 100), blockListener);
+            }
+
+            // 发布事件
+            if (isPublishUserEvent) {
+                for (Map.Entry<Bytes, String[]> kv : eventNames.entrySet()) {
+                    for (int m = 0; m < kv.getValue().length; m++) {
+                        Bytes da = kv.getKey();
+                        for (int j = -1; j < NEW_EVENT_SEQUENCE_SIZE - 1; j++) {
+                            IntegrationBase.EventResponse eventResponse = IntegrationBase.testSDK_PublishEvent(adminKey, ledgerHash, blockchainService, da, kv.getValue()[m], j);
+                            Thread.sleep(100);
+                            validEventPublish(eventResponse, ledgerRepository, blockchainService);
+                        }
+                    }
+                }
+            }
+
+            while (blockListener != null) {
+                Thread.sleep(1000);
+                if(blockListener.height.get() == blockchainService.getLedger(ledgerHash).getLatestBlockHeight()) {
+                    blockListenerHandler.cancel();
+                    break;
+                }
+            }
         }
+
+        System.out.println("----------------- Init Completed -----------------");
+        IntegrationBase.testConsistencyAmongNodes(ledgers);
+//        Thread.sleep(Integer.MAX_VALUE);
 
     }
     private HashDigest initLedger(String[] dbConnections) {
@@ -216,5 +347,77 @@ public class IntegrationTest4Operations {
         HashDigest ledgerHash = ledgerInit.testInitWith4Nodes(LedgerInitConsensusConfig.bftsmartConfig, dbConnections);
         System.out.printf("LedgerHash = %s \r\n", ledgerHash.toBase58());
         return ledgerHash;
+    }
+
+    class BlockchainUserEventListener implements UserEventListener<UserEventPoint> {
+
+        int totalCount = 0;
+        AtomicInteger count = new AtomicInteger(0);
+
+        Map<Bytes, Map<String, Long>> points = new HashMap<>();
+        Map<Bytes, Map<String, Boolean>> status = new HashMap<>();
+
+        public void register(Bytes eventAccount, String eventName, long maxSequence, int count) {
+            if (!points.containsKey(eventAccount)) {
+                points.put(eventAccount, new HashMap<>());
+                status.put(eventAccount, new ConcurrentHashMap<>());
+            }
+
+            if (!points.get(eventAccount).containsKey(eventName)) {
+                points.get(eventAccount).put(eventName, maxSequence);
+                status.get(eventAccount).put(eventName, false);
+            }
+
+            totalCount += count;
+        }
+
+        @Override
+        public void onEvent(Event eventMessage, EventContext<UserEventPoint> eventContext) {
+            count.incrementAndGet();
+            Object content;
+            if (eventMessage.getContent().getType() == DataType.TEXT) {
+                content = eventMessage.getContent().getBytes().toUTF8String();
+            } else if (eventMessage.getContent().getType() == DataType.BYTES) {
+                content = new String(eventMessage.getContent().getBytes().toBytes());
+            } else {
+                content = BytesUtils.toLong(eventMessage.getContent().getBytes().toBytes());
+            }
+//            System.out.println("eventAccount:" + eventMessage.getEventAccount().toBase58() + ", name:" + eventMessage.getName() + ", sequence:" + eventMessage.getSequence() + ", content:" + content);
+            assertTrue(eventMessage.getSequence() <= points.get(eventMessage.getEventAccount()).get(eventMessage.getName()));
+            if (eventMessage.getSequence() == points.get(eventMessage.getEventAccount()).get(eventMessage.getName())) {
+                status.get(eventMessage.getEventAccount()).put(eventMessage.getName(), true);
+            }
+            if(finish()) {
+                assertEquals(count.intValue(), totalCount);
+                eventContext.getHandle().cancel();
+            }
+        }
+
+        private boolean finish() {
+            boolean finish = true;
+            for(Map.Entry<Bytes, Map<String, Boolean>> kv : status.entrySet()) {
+                for(boolean status : kv.getValue().values()) {
+                    finish = finish && status;
+                    if(!finish) {
+                        return false;
+                    }
+                }
+            }
+
+            return true;
+        }
+    }
+
+    class BlockchainNewBlockEventListener implements SystemEventListener<SystemEventPoint> {
+
+        private AtomicInteger height = new AtomicInteger(-1);
+
+        @Override
+        public void onEvents(Event[] eventMessages, EventContext<SystemEventPoint> eventContext) {
+            for(Event eventMessage : eventMessages) {
+                height.incrementAndGet();
+                System.out.println("name:" + eventMessage.getName() + ", sequence:" + eventMessage.getSequence() + ", content:" + eventMessage.getContent().getBytes().toUTF8String());
+            }
+        }
     }
 }
